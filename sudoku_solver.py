d2d6aac555b8131c63e6c6d0c11402ec06fdd646
Andrey Semakin <and-semakin@ya.ru>
2019-11-09 20:29:06 +0500

    Add function to extract row, column and square

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,4 +1,5 @@
 import copy
+import math
 from typing import List
 
 Sudoku = List[List[int]]
@@ -10,6 +11,30 @@ class SudokuError(Exception):
 
 def sudoku_solved(puzzle: Sudoku) -> bool:
     return all(all(row) for row in puzzle)
+
+
+def get_row(puzzle: Sudoku, i: int) -> List[int]:
+    return puzzle[i]
+
+
+def get_column(puzzle: Sudoku, i: int) -> List[int]:
+    return [row[i] for row in puzzle]
+
+
+def get_square(puzzle: Sudoku, i: int) -> List[int]:
+    size = len(puzzle)
+    if not math.sqrt(size).is_integer():
+        raise ValueError("Sudoku can not be splitted to little squares")
+
+    little_square_size = int(math.sqrt(size))
+
+    x, y = divmod(i, little_square_size)
+    square = []
+
+    for row in puzzle[x * little_square_size : (x + 1) * little_square_size]:
+        square.extend(row[y * little_square_size : (y + 1) * little_square_size])
+
+    return square
 
 
 def sudoku(puzzle: Sudoku) -> Sudoku:
--- test_unit.py
+++ test_unit.py
@@ -1,6 +1,8 @@
+from typing import List
+
 import pytest
 
-from sudoku_solver import Sudoku, sudoku_solved
+from sudoku_solver import Sudoku, sudoku_solved, get_row, get_column, get_square
 
 
 class TestUnit:
@@ -14,3 +16,62 @@ class TestUnit:
     def test_sudoku_solved(self, puzzle: Sudoku, solved: bool) -> None:
         assert sudoku_solved(puzzle) == solved
 
+    @pytest.mark.parametrize(
+        ("puzzle", "row_number", "expected_row"),
+        [
+            ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 0, [1, 2, 3],),
+            ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 1, [4, 5, 6],),
+            ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 2, [7, 8, 9],),
+        ],
+    )
+    def test_get_row(
+        self, puzzle: Sudoku, row_number: int, expected_row: List[int]
+    ) -> None:
+        assert get_row(puzzle, row_number) == expected_row
+
+    @pytest.mark.parametrize(
+        ("puzzle", "column_number", "expected_column"),
+        [
+            ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 0, [1, 4, 7],),
+            ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 1, [2, 5, 8],),
+            ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 2, [3, 6, 9],),
+        ],
+    )
+    def test_get_column(
+        self, puzzle: Sudoku, column_number: int, expected_column: List[int]
+    ) -> None:
+        assert get_column(puzzle, column_number) == expected_column
+
+    @pytest.mark.parametrize(
+        ("puzzle", "square_number", "expected_square"),
+        [
+            (
+                [[1, 1, 2, 2], [1, 1, 2, 2], [3, 3, 4, 4], [3, 3, 4, 4]],
+                0,
+                [1, 1, 1, 1],
+            ),
+            (
+                [[1, 1, 2, 2], [1, 1, 2, 2], [3, 3, 4, 4], [3, 3, 4, 4]],
+                1,
+                [2, 2, 2, 2],
+            ),
+            (
+                [[1, 1, 2, 2], [1, 1, 2, 2], [3, 3, 4, 4], [3, 3, 4, 4]],
+                2,
+                [3, 3, 3, 3],
+            ),
+            (
+                [[1, 1, 2, 2], [1, 1, 2, 2], [3, 3, 4, 4], [3, 3, 4, 4]],
+                3,
+                [4, 4, 4, 4],
+            ),
+        ],
+    )
+    def test_get_square(
+        self, puzzle: Sudoku, square_number: int, expected_square: List[int]
+    ) -> None:
+        assert get_square(puzzle, square_number) == expected_square
+
+    def test_get_square_raises_if_puzzle_can_not_be_split_to_squares(self) -> None:
+        with pytest.raises(ValueError):
+            get_square([[1], [2], [3]], 1)
